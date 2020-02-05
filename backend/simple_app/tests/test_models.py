3e177aed6dbc3017da294af410b242b33e403430
neo1 <tobby2002@gmailcom>
2020-02-06 00:06:51 +0900

    init django3 python3.7

--- backend/simple_app/tests/test_models.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from mixer.backend.django import mixer
-
-pytestmark = pytest.mark.django_db
-
-
-def test_message():
-    obj = mixer.blend('simple_app.Message')
-    assert obj.pk > 0
